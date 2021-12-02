9c9cec00a65a20291f189fe46abfb42a6bbea073
Julien Deramaix <jderamaix@iutbayonne.univ-pau.fr>
2021-12-02 09:05:34 +0100

    Création des routes demandées

--- src/Controller/ProStageController.php
+++ src/Controller/ProStageController.php
@@ -11,8 +11,37 @@ class ProStageController extends AbstractController
     /**
      * @Route("/", name="bienvenu")
      */
-	
     public function index(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('pro_stage/index.html.twig', [
+            'controller_name' => 'ProStageController',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="stage")
+     */
+    public function stage(): Response
     {
         return $this->render('pro_stage/index.html.twig', [
             'controller_name' => 'ProStageController',
