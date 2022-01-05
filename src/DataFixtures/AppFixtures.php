423ae799b67bfe8aef00410db01689993737a772
Julien Deramaix <jderamaix@iutbayonne.univ-pau.fr>
2022-01-05 12:34:37 +0100

    Faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,6 +15,22 @@ class AppFixtures extends Fixture
         // Création d'un générateur de données avec la librairie Faker
         $faker = \Faker\Factory::create('fr_FR');
 
+        
+        
+        //Création de 15 entreprises
+        for($i = 0; $i < 16 ; $i++)
+        {
+            // Création module Entreprise
+            $entreprise = new Entreprise();
+            $entreprise->setNom($faker->company);
+            $entreprise->setAdresse($faker->address);
+            $entreprise->setActivite($faker->sentence($nbWords = 1, $variableNbWords = true));
+            $entreprise->setURLsite($faker->url);
+            $entreprises[] = $entreprise;
+            $manager->persist($entreprise);
+            
+        }
+
         /*****************************
          ***  LISTE DES FORMATIONS ***
          *****************************/
@@ -24,27 +40,13 @@ class AppFixtures extends Fixture
             "LP PROG " => "Licence Professionels - Programmation Avancee",
             "LP NUM" => "Licence Professionnels - Numérique"
             );
-        
-        //Création de 15 entreprises
-        for($i = 0; $i < 16 ; $i++)
-        {
-            // Création module Entreprise
-            $entreprise = new Entreprise();
-            $entreprise->setNom($faker->company);
-            $entreprise->setAdresse($faker->adress);
-            $entreprise->setActivite($faker->sentence($nbWords = 1, $variableNbWords = true));
-            $entreprise->setSiteURL($fake->url);
-            $entreprise[] = $entreprise;
-            $manager->persist($entreprise);
-            
-        }
-        
+
         foreach($formationListe as $nomCourtFormation => $nomLongFormation)
         {
             // Création module Formation
             $formation = new Formation();
-            $Formation->setNomLong($nomLongFormation);
-            $Formation->setNomCourt($nomCourtFormation);
+            $formation->setNomLong($nomLongFormation);
+            $formation->setNomCourt($nomCourtFormation);
             
             //Création de 15 stages
             for($j = 0 ; $j < 16 ; $j++)
@@ -53,17 +55,17 @@ class AppFixtures extends Fixture
                 $stage = new Stage();
                 $stage->setTitre($faker->sentence($nbWords = 6, $variableNbWords = true));
                 $stage->setMission($faker->realText($maxNbChars = 200, $indexSize = 2));
-                $stage->setMail($faker->ascii_company_email());
+                $stage->setEmail($faker->email);
                 
                 //Relation entre Formation et Stage
-               $stage->addConcerne($formation);
+               $stage->addFormation($formation);
 
                $numEntreprise = $faker->numberBetween($min =0,$max=19);
-               $stage->setEstAjoutePar($entreprises[$numEntreprise]);
-               $entreprises[$numEntreprise]->addStage($stage);
+               $stage->setEntreprise($entreprise[$numEntreprise]);
+               $entreprise[$numEntreprise]->addStage($stage);
 
                 $manager->persist($stage);
-                $manager->persist($entreprises[$numEntreprise]);
+                $manager->persist($entreprise[$numEntreprise]);
             }
             $manager->persist($formation);
         }
